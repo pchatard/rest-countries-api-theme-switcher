c4772290c5615485591e3c44a92e1745db740242
Shay Reichert <shay.reichert@outlook.fr>
2020-11-14 09:25:38 +0100

    [JS] background color on theme change (#09) - Set the background color body

--- js/index.js
+++ js/index.js
@@ -11,6 +11,20 @@ darkModeSwitch.addEventListener("change", handleThemeSwitch);
 function handleThemeSwitch(event) {
 	const newValue = event.target.checked;
 	localStorage.setItem("darkMode", newValue);
+	handleBackgroundColor();
+}
+
+// ***************************************************************
+
+// Set the background color body on page load
+isDarkMode && handleBackgroundColor();
+
+// Set the background color body on theme switch
+function handleBackgroundColor() {
+	const backgroundgBody = document.body.classList;
+	darkModeSwitch.checked
+		? backgroundgBody.add("dark-background")
+		: backgroundgBody.remove("dark-background");
 }
 
 // ***************************************************************
