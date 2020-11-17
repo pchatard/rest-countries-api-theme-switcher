10647d0890721aac3c0049b6db3495b95e62624e
Shay <63343303+ShayReichert@users.noreply.github.com>
2020-11-17 09:23:37 +0100

    [GLOBAL & JS] Modal and Homepage - Files merge and open modal
    * [GLOBAL & JS] Modal and Homepage - Merge of the files and opening modal
    
    * [GLOBAL & JS] Modal and Homepage (#5) - Fix issues and add border countries eventListener
    
    * [GLOBAL & JS] Modal and Homepage (#5) - correction function
    
    Co-authored-by: Shay Reichert <shay.reichert@outlook.fr>

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+// ******************************* DARKMODE ******************************* //
+
 const darkModeSwitch = document.getElementById("theme");
 
 // Set the theme on page load from Local storage
 const isDarkMode = localStorage.getItem("darkMode");
-console.log(JSON.parse(isDarkMode));
+// console.log(JSON.parse(isDarkMode));
 darkModeSwitch.checked = JSON.parse(isDarkMode);
 
 // Keep track of every theme switch in the local storage
@@ -14,7 +16,7 @@ function handleThemeSwitch(event) {
 	handleBackgroundColor(newThemeValue);
 }
 
-// ***************************************************************
+// ******************************
 
 // Set the background color body on page load
 handleBackgroundColor(JSON.parse(isDarkMode));
@@ -27,4 +29,38 @@ function handleBackgroundColor(isDarkMode = true) {
 		: backgroundBody.remove("dark-background");
 }
 
-// ***************************************************************
+// ******************************* API ******************************* //
+// Api fetching here
+
+// ******************************* MODAL ******************************* //
+
+const modal = document.querySelector(".modal-container");
+const countries = document.querySelectorAll(".country__container");
+const main = document.querySelector("main");
+const buttonBack = document.querySelector(".modal__btn");
+const borderCountries = document.querySelectorAll(".border-countries__btn");
+
+// On click on country card, open modal
+countries.forEach((country) => {
+	country.addEventListener("click", getCountryInfos);
+});
+
+function getCountryInfos() {
+	// Add more logic here to transmit country info, then...
+	openModal();
+}
+
+function openModal() {
+	main.classList.add("disappear");
+}
+
+// On click on the back button, close modal
+buttonBack.addEventListener("click", closeModal);
+function closeModal() {
+	main.classList.remove("disappear");
+}
+
+// On click on "border-countries__btn", open the appropriate detail card
+borderCountries.forEach((borderCountry) => {
+	borderCountry.addEventListener("click", getCountryInfos);
+});
